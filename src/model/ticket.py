c491d543df2b2ea0cb1f8dd021c20dd844015e08
Carter <speers.carter@gmail.com>
2026-01-25 12:07:51 -0500

    wip: user registration
    I added a security module, and I'm working on user registration. I also fixed the model for the ticket data to use enums for status and priority

--- src/model/ticket.py
+++ src/model/ticket.py
@@ -1,9 +1,20 @@
+from enum import Enum
 from pydantic import BaseModel, Field
 
+class Priority(str, Enum):
+    low = "low"
+    medium = "medium"
+    high = "high"
+
+class Status(str, Enum):
+    not_started = "not started"
+    in_progress = "in progress"
+    finished = "finished"
 
 class Ticket(BaseModel):
     id: int
     subject: str = Field(..., min_length=2, max_length=50)
     description: str = Field(..., min_length=25, max_length=500)
-    priorty: str
-    status: str
+    owner: str = Field(..., min_length=2, max_length=30)
+    priorty: Priority = medium
+    status: Status = not_started
--- src/security/security.py
+++ src/security/security.py
@@ -1,58 +1,153 @@
+from datetime import datetime, timedelta, timezone
+from typing import Annotated
+
+import jwt
+from fastapi import Depends, FastAPI, HTTPException, status, APIRouter
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jwt.exceptions import InvalidTokenError
+from pwdlib import PasswordHash
 from pydantic import BaseModel
-from fastapi import Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
+
+# to get a string like this run:
+# openssl rand -hex 32
+SECRET_KEY = "050ed97a7553ff5c4725ca1ff659d45c063976786d43ff7b1d0ddf968c041db0"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
 
 fake_users_db = {
     "johndoe": {
         "username": "johndoe",
-        "hashed_password": "hashedsecret"
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "$argon2id$v=19$m=65536,t=3,p=4$wagCPXjifgvUFBzq4hqe3w$CYaIb8sB+wtD+Vu/P4uod1+Qof8h+1g7bbDlBID48Rc",
+        "disabled": False,
     },
-    "janedoe": {
-        "username": "janedoe",
-        "hashed_password": "hashedsecret2"
+    "carter": {
+        "username": "carter",
+        "full_name": "Carter Spe",
+        "email": "speers-carter@gmail.com",
+        "hashed_password": "$argon2id$v=19$m=65536,t=3,p=4$47jnnv0qAtf8bJGrS0UEnA$eS5ESEcV0xbhATcMs9AXnbozaBeUIblL19Kizql+OeY",
+        "disabled": False,
     }
 }
 
 
+class Token(BaseModel):
+    access_token: str
+    token_type: str
 
-def fakely_hash_password(password: str): return f"hashed{password}"
+
+class TokenData(BaseModel):
+    username: str | None = None
+
 
 class User(BaseModel):
     username: str
+    email: str | None = None
+    full_name: str | None = None
+    disabled: bool | None = None
+
 
 class UserInDB(User):
     hashed_password: str
+
+
+password_hash = PasswordHash.recommended()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+router = APIRouter();
+
+def verify_password(plain_password, hashed_password):
+    return password_hash.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return password_hash.hash(password)
+
 
 def get_user(db, username: str):
     if username in db:
         user_dict = db[username]
         return UserInDB(**user_dict)
 
-def fake_token_generator(user: UserInDB) -> str:
-    # This doesn't provide any security at all
-    return f"tokenized{user.username}"
+
+def authenticate_user(fake_db, username: str, password: str):
+    user = get_user(fake_db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
 
 
-def fake_token_resolver(
-    token: str
-) -> UserInDB | None:
-    if token.startswith("tokenized"):
-        user_id = token.removeprefix("tokenized")
-        user = get_user(fake_users_db, user_id)
-        return user
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-def get_user_from_token(
-    token: str = Depends(oauth2_scheme),
-) -> UserInDB:
-    user = fake_token_resolver(token)
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except InvalidTokenError:
+        raise credentials_exception
+    user = get_user(fake_users_db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+async def get_current_active_user(
+    current_user: Annotated[User, Depends(get_current_user)],
+):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+
+@router.post("/token")
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+) -> Token:
+    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail=(
-                "Invalid authentication credentials"
-            ),
+            detail="Incorrect username or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
-    return user
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return Token(access_token=access_token, token_type="bearer")
+
+
+@router.get("/users/me/", response_model=User)
+async def read_users_me(
+    current_user: Annotated[User, Depends(get_current_active_user)],
+):
+    return current_user
+
+
+@router.get("/users/me/items/")
+async def read_own_items(
+    current_user: Annotated[User, Depends(get_current_active_user)],
+):
+    return [{"item_id": "Foo", "owner": current_user.username}]
